b3647280449f00aea52db31a6901994a9f94f961
Tobias235 <tobias.prepas@gmail.com>
2022-05-18 22:06:12 +0800

    Added background picture

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import styles from "./App.module.scss";
+import NavbarContainer from "./containers/NavbarContainer";
 
 function App() {
-  return <div className={styles.app}></div>;
+  return <div className={styles.app}>{/* <NavbarContainer /> */}</div>;
 }
 
 export default App;
