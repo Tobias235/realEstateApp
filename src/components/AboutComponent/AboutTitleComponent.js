2b3e77ac4c10de6360cc79fdf92b8782e0a3fca5
Tobias235 <tobias.prepas@gmail.com>
2022-06-01 19:54:00 +0800

    Revert "Added background picture to h1. Fixed styling"

---
+++ src/components/AboutComponent/AboutTitleComponent.js
@@ -0,0 +1,11 @@
+import styles from "./AboutTitleComponent.module.scss";
+
+const AboutTitleComponent = () => {
+  return (
+    <div className={styles.aboutTitle}>
+      <h1>About PRESMIY</h1>
+    </div>
+  );
+};
+
+export default AboutTitleComponent;
