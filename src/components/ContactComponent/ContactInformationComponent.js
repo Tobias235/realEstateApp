dab1b67471edd6036373d1ad1aefb0f21b92429c
Tobias235 <tobias.prepas@gmail.com>
2022-06-03 21:10:44 +0800

    Updated styling

--- src/components/ContactComponent/ContactInformationComponent.js
+++ src/components/ContactComponent/ContactInformationComponent.js
@@ -1,13 +1,23 @@
+import { FaRegEnvelope } from "react-icons/fa";
+import { FiPhone } from "react-icons/fi";
+
 import styles from "./ContactInformationComponent.module.scss";
 import contactUs from "../../assets/images/contactUs.webp";
 
 const ContactInformationComponent = () => {
   return (
     <div className={styles.contactInformation}>
-      <p>E-Mail:</p>
-      <span>companyemail@yourdomain.com</span>
+      <a href="%" className={styles.email}>
+        <FaRegEnvelope />
+        <span>Write us an email</span>
+      </a>
+      <a href="%" className={styles.phone}>
+        <FiPhone />
+        <span>Call us at +123456789</span>
+      </a>
+      {/* <span>companyemail@yourdomain.com</span>
       <p>Phone:</p>
-      <span>123456789</span>
+      <span>123456789</span> */}
 
       <img src={contactUs} alt="Contact us" />
     </div>
