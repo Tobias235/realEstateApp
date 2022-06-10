ebf241df96e4743374278f797aedd6b8751d1657
Tobias235 <tobias.prepas@gmail.com>
2022-06-10 17:34:52 +0800

    Updated styling for small screens

---
+++ src/components/HomeComponent/HomeTextComponent.js
@@ -0,0 +1,12 @@
+import styles from "./HomeTextComponent.module.scss";
+
+const HomeTextComponent = () => {
+  return (
+    <>
+      <h1>Take a step farther to find a new home</h1>
+      <h2>Contact us and wake up in a new home tomorrow!</h2>
+    </>
+  );
+};
+
+export default HomeTextComponent;
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,12 +1,14 @@
 import HomeButtonComponent from "../components/HomeComponent/HomeButtonComponent";
+import HomeTextComponent from "../components/HomeComponent/HomeTextComponent";
 import styles from "./HomeContainer.module.scss";
 
 const HomeContainer = () => {
   return (
     <div className={styles.home}>
-      <h1>Take a step farther to find a new home</h1>
-      <h2>Contact us and wake up in a new home tomorrow!</h2>
-      <HomeButtonComponent />
+      <div className={styles.componentContainer}>
+        <HomeTextComponent />
+        <HomeButtonComponent />
+      </div>
     </div>
   );
 };
