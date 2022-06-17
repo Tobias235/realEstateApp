adb08cf5bbfb2dbe1e481fb57ba541929a95e27b
Tobias235 <tobias.prepas@gmail.com>
2022-06-17 18:11:57 +0800

    Added functionality to mobile navbar

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import styles from "./App.module.scss";
 import PropertiesContainer from "./containers/PropertiesContainer";
 import AboutContainer from "./containers/AboutContainer";
@@ -5,11 +7,29 @@ import ContactContainer from "./containers/ContactContainer";
 import HomeContainer from "./containers/HomeContainer";
 import NavbarContainer from "./containers/NavbarContainer";
 import Footer from "./components/Footer";
+import PropertiesModal from "./components/PropertiesComponent/PropertiesModal";
+import MobileNav from "./components/NavBarComponent/MobileNavbarComponents/MobileNav";
+import MobileNavbar from "./components/NavBarComponent/MobileNavbarComponents/MobileNavbar";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    if (showMenu) {
+      setShowMenu(false);
+      return;
+    }
+    setShowMenu(true);
+  };
+
+  const handleCloseMenu = () => {
+    setShowMenu(false);
+  };
   return (
     <>
       <NavbarContainer />
+      <MobileNavbar onShow={handleShowMenu} onClose={handleCloseMenu} />
+      {showMenu && <MobileNav onClose={handleCloseMenu} />}
       <HomeContainer />
       <PropertiesContainer />
       <AboutContainer />
