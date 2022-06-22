18d84bb527e981e25b2a67ff5b97c1d0a088a466
Tobias235 <tobias.prepas@gmail.com>
2022-06-23 00:45:42 +0800

    Updated styling

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -5,6 +5,7 @@ import styles from "./AboutContainer.module.scss";
 const AboutContainer = () => {
   return (
     <div id="about" className={styles.about}>
+      <div className={styles.spaceTop}></div>
       <AboutTitleComponent />
       <AboutUsComponent />
     </div>
