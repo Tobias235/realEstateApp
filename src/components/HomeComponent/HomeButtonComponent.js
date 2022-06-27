8c9f34b6c600f63f2ece8170b5c99979a1d9dfcb
Tobias235 <tobias.prepas@gmail.com>
2022-06-27 20:10:28 +0800

    Fixed buttons

--- src/components/HomeComponent/HomeButtonComponent.js
+++ src/components/HomeComponent/HomeButtonComponent.js
@@ -3,8 +3,8 @@ import styles from "./HomeButtonComponent.module.scss";
 const HomeButtonComponent = () => {
   return (
     <div className={styles.homeButtons}>
-      <button>View all properties</button>
-      <button>Contact Us</button>
+      <a href="#properties">View all properties</a>
+      <a href="#contact">Contact Us</a>
     </div>
   );
 };
