3a16f3d3d67ab1abad7404687144b72a81a16880
Tobias235 <tobias.prepas@gmail.com>
2022-08-25 20:35:49 +0800

    Conditional rendering for comment

--- src/containers/PropertiesModalContainer.js
+++ src/containers/PropertiesModalContainer.js
@@ -1,12 +1,17 @@
 import styles from "./PropertiesModalContainer.module.scss";
 import LeftModal from "../components/PropertiesComponent/PropertiesModalComponent/LeftModal";
 import RightModal from "../components/PropertiesComponent/PropertiesModalComponent/RightModal";
+import AddCommentComponent from "../components/PropertiesComponent/PropertiesModalComponent/PropertiesCommentModal/AddCommentComponent";
+import { useSelector } from "react-redux";
 
 const PropertiesModalContainer = ({ onClose }) => {
+  const addComments = useSelector((state) => state.show_comment);
+
   return (
     <div className={styles.modal}>
-      <LeftModal />
-      <RightModal onClose={onClose} />
+      {!addComments && <LeftModal />}
+      {!addComments && <RightModal onClose={onClose} />}
+      {addComments && <AddCommentComponent />}
     </div>
   );
 };
