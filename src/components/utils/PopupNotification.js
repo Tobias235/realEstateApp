3f9afe043555ec1ba819d16a0cdd6cb6aea00516
Tobias235 <tobias.prepas@gmail.com>
2022-08-26 18:31:09 +0800

    Created login notification

---
+++ src/components/utils/PopupNotification.js
@@ -0,0 +1,29 @@
+import styles from "./PopupNotification.module.scss";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
+const PopupNotifications = () => {
+  const [message, setMessage] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
+  const loginStatus = useSelector((state) => state.login_status);
+  const currentUser = useSelector((state) => state.current_user);
+
+  useEffect(() => {
+    if (loginStatus) {
+      setShowMessage(true);
+      setMessage(`Welcome, ${currentUser}`);
+    }
+    const timeout = setTimeout(() => {
+      setShowMessage(false);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [loginStatus]);
+
+  const style = showMessage ? styles.notification : styles.hidden;
+  return <span className={style}>{message}</span>;
+};
+
+export default PopupNotifications;
