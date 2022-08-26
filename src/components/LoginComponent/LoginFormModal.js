e5142823df13170d1ce4a8dbe80f7a65a631b02b
Tobias235 <tobias.prepas@gmail.com>
2022-08-26 18:32:13 +0800

    Error handling
    Set up styling for errors and added display for errors

--- src/components/LoginComponent/LoginFormModal.js
+++ src/components/LoginComponent/LoginFormModal.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../Firebase";
@@ -9,10 +9,16 @@ import {
   setShowLoginModal,
 } from "../../actions/Actions";
 import { useDispatch } from "react-redux";
+import Button from "../utils/Button";
+import styles from "./LoginFormModal.module.scss";
 
 const LoginFormModal = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [currentErrorMessage, setCurrentErrorMessage] = useState(null);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [userNotFound, setUserNotFound] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -29,28 +35,68 @@ const LoginFormModal = () => {
       dispatch(setShowLoginModal(false));
       dispatch(setBackground(false));
     } catch (error) {
-      console.log(error.message);
+      setCurrentErrorMessage(error);
     }
   };
+
+  useEffect(() => {
+    if (currentErrorMessage === null) {
+      return;
+    }
+    const errorString = currentErrorMessage.toString().split("/");
+    const error = errorString[1].replace(/[^a-zA-Z0-9 ]/g, " ").trim();
+    console.log(error);
+    setEmailError(
+      error === "invalid email" ||
+        error === "user not found" ||
+        error === "internal error"
+        ? true
+        : false
+    );
+    setUserNotFound(error === "user not found" ? true : false);
+    setPasswordError(
+      error === "wrong password" || error === "internal error" ? true : false
+    );
+  }, [currentErrorMessage]);
+
+  const passwordText = passwordError ? styles.errorText : null;
+  const mailText = emailError ? styles.errorText : null;
+
+  const errorEmail = emailError && (
+    <span className={mailText}>
+      {userNotFound
+        ? "User Not Found, sign up below"
+        : "Please enter a valid e-mail"}
+    </span>
+  );
+
+  const errorPassword = passwordError && (
+    <span className={passwordText}>Please enter correct password</span>
+  );
+
   return (
-    <form onSubmit={handleUserLogin}>
-      <label>Enter Your email:</label>
+    <form onSubmit={handleUserLogin} className={styles.logInForm}>
+      <label className={mailText}>Enter Your email:</label>
       <input
         type="email"
         placeholder="Enter Your email:"
+        className={emailError ? styles.error : null}
         onChange={(e) => {
           setLoginEmail(e.target.value);
         }}
       />
-      <label>Enter Your Password:</label>
+      {errorEmail}
+      <label className={passwordText}>Enter Your Password:</label>
       <input
         type="password"
         placeholder="Enter Your Password:"
+        className={passwordError ? styles.error : null}
         onChange={(e) => {
           setLoginPassword(e.target.value);
         }}
       />
-      <button type="submit">Sign In</button>
+      {errorPassword}
+      <Button type="submit" text="Sign In" className={styles.signInButton} />
     </form>
   );
 };
