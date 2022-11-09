6b500327df63e70284519c53f1fb311c8ddd8bfd
Tobias235 <tobias.prepas@gmail.com>
2022-11-09 11:23:15 +0100

    Added functianlity for add comment

--- src/components/PropertiesComponent/PropertiesModalComponent/PropertiesCommentModal/AddCommentComponent.js
+++ src/components/PropertiesComponent/PropertiesModalComponent/PropertiesCommentModal/AddCommentComponent.js
@@ -1,25 +1,50 @@
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 import { setShowAddComment } from "../../../../actions/Actions";
 import Button from "../../../utils/Button/Button";
-import styles from "./AddCommentComponent.module.scss";
 import CommentBox from "./CommentBox";
+import styles from "./AddCommentComponent.module.scss";
+
 const AddCommentComponent = () => {
   const dispatch = useDispatch();
+  const textRef = useRef("");
 
   const handleCloseComment = () => {
     dispatch(setShowAddComment(false));
+  };
+
+  const handleAddComment = (e) => {
+    console.log(e.target.reset);
+    console.log(textRef.current.value);
+    textRef.current.value = "";
   };
 
   return (
     <div className={styles.addComment}>
       <h1>Add your comment</h1>
       <CommentBox className={styles.comments} />
-      <textarea rows="6"></textarea>
-      <Button
-        text="Close"
-        className={styles.closeButton}
-        onClick={handleCloseComment}
-      />
+      <form className={styles.textForm}>
+        <label>Your comment:</label>
+        <textarea
+          type="text"
+          rows="5"
+          placeholder="Write your comment..."
+          ref={textRef}
+        ></textarea>
+      </form>
+      <div className={styles.buttonContainer}>
+        <Button
+          text="Close"
+          className={styles.closeButton}
+          onClick={handleCloseComment}
+        />
+        <Button
+          type="Submit"
+          text="Add Comment"
+          className={styles.closeButton}
+          onClick={handleAddComment}
+        />
+      </div>
     </div>
   );
 };
