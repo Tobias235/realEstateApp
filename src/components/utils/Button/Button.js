35f47c30b11cc93aee528a7e7f6e30f14319b83f
Tobias235 <tobias.prepas@gmail.com>
2022-11-10 14:13:29 +0100

    Added disabled to re-useable button

--- src/components/utils/Button/Button.js
+++ src/components/utils/Button/Button.js
@@ -1,11 +1,12 @@
 import styles from "./Button.module.scss";
 
-const Button = ({ type, text, className, onClick }) => {
+const Button = ({ type, text, className, onClick, disabled }) => {
   return (
     <button
       type={type}
       className={`${styles.button} ${className}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
