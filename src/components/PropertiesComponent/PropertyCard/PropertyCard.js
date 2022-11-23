7f81c768fca9bfb1d0fec376b54255c3d0506757
Tobias235 <tobias.prepas@gmail.com>
2022-11-23 14:49:43 +0100

    Clean up
    Moved functionality into the property card component

--- src/components/PropertiesComponent/PropertyCard/PropertyCard.js
+++ src/components/PropertiesComponent/PropertyCard/PropertyCard.js
@@ -1,7 +1,13 @@
+import { useDispatch } from "react-redux";
+import { setShowDetails } from "../../../actions/Actions";
 import placeholder from "../../../assets/images/placeholderProperties.webp";
 import styles from "./PropertyCard.module.scss";
 
-const PropertyCard = ({ onShowDetails }) => {
+const PropertyCard = () => {
+  const dispatch = useDispatch();
+  const handleShowDetails = () => {
+    dispatch(setShowDetails(true));
+  };
   return (
     <div className={styles.properties}>
       <img src={placeholder} alt="Placeholder for properties" />
@@ -15,7 +21,7 @@ const PropertyCard = ({ onShowDetails }) => {
         <span>sqm: 100</span>
       </div>
       <div className={styles.buttonContainer}>
-        <button onClick={onShowDetails}>More Details</button>
+        <button onClick={handleShowDetails}>More Details</button>
       </div>
     </div>
   );
--- src/containers/PropertiesContainer/PropertiesContainer.js
+++ src/containers/PropertiesContainer/PropertiesContainer.js
@@ -1,38 +1,22 @@
 import PropertiesFilter from "../../components/PropertiesComponent/PropertiesFilter/PropertiesFilter";
 import PropertyCard from "../../components/PropertiesComponent/PropertyCard/PropertyCard";
-import PropertiesModalContainer from "./../PropertiesModalContainer/PropertiesModalContainer";
-import { useDispatch, useSelector } from "react-redux";
-import { setBackground, setShowDetails } from "../../actions/Actions";
+
 import styles from "./PropertiesContainer.module.scss";
 
 const PropertiesContainer = () => {
-  const showModal = useSelector((state) => state.show_details);
-  const dispatch = useDispatch();
-
-  const handleShowDetails = () => {
-    dispatch(setBackground(true));
-    dispatch(setShowDetails(true));
-  };
-
-  const handleCloseDetails = () => {
-    dispatch(setBackground(false));
-    dispatch(setShowDetails(false));
-  };
-
   return (
     <div id="properties" className={styles.properties}>
-      {showModal && <PropertiesModalContainer onClose={handleCloseDetails} />}
       <h1 className={styles.propertyHeader}>Our Properties</h1>
       <PropertiesFilter />
       <div className={styles.grid}>
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
-        <PropertyCard onShowDetails={handleShowDetails} />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
+        <PropertyCard />
       </div>
     </div>
   );
