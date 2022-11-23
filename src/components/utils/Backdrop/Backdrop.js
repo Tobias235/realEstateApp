d1c294b5d19fd03ed510a185968c9f6ff9ff069e
Tobias235 <tobias.prepas@gmail.com>
2022-11-23 14:46:11 +0100

    Added backdrop with create portal

---
+++ src/components/utils/Backdrop/Backdrop.js
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import ReactDOM from "react-dom";
+import {
+  setMobileMenu,
+  setShowDetails,
+  setShowLoginModal,
+  setShowMobileNavOptions,
+  setShowPropertiesModal,
+} from "../../../actions/Actions";
+import styles from "./Backdrop.module.scss";
+
+const Backdrop = () => {
+  const dispatch = useDispatch();
+  const portalElement = document.getElementById("overlays");
+
+  const onClose = () => {
+    dispatch(setShowLoginModal(false));
+    dispatch(setShowDetails(false));
+    dispatch(setShowPropertiesModal(false));
+    dispatch(setMobileMenu(false));
+    dispatch(setShowMobileNavOptions(false));
+  };
+
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div className={styles.background} onClick={onClose}></div>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Backdrop;
