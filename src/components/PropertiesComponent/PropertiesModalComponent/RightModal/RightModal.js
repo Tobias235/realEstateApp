1f04c20dae7209b5935bad0706953dea612e2097
Tobias235 <tobias.prepas@gmail.com>
2022-11-24 11:00:40 +0100

    Moved functionality into component

--- src/components/PropertiesComponent/PropertiesModalComponent/RightModal/RightModal.js
+++ src/components/PropertiesComponent/PropertiesModalComponent/RightModal/RightModal.js
@@ -1,8 +1,16 @@
-import styles from "./RightModal.module.scss";
+import { setShowDetails } from "../../../../actions/Actions";
+import { useDispatch } from "react-redux";
 import ModalDescription from "../ModalDescription/ModalDescription";
 import Button from "../../../utils/Button/Button";
+import styles from "./RightModal.module.scss";
 
-const RightModal = ({ onClose }) => {
+const RightModal = () => {
+  const dispatch = useDispatch();
+
+  const handleCloseDetails = () => {
+    dispatch(setShowDetails(false));
+  };
+
   return (
     <div className={styles.rightModal}>
       <h1>Description</h1>
@@ -10,7 +18,7 @@ const RightModal = ({ onClose }) => {
       <div className={styles.buttonContainer}>
         <Button
           type="button"
-          onClick={onClose}
+          onClick={handleCloseDetails}
           text="Close Details"
           className={styles.closeButton}
         />
--- src/components/PropertiesModal/PropertiesModal.js
+++ src/components/PropertiesModal/PropertiesModal.js
@@ -1,22 +1,16 @@
 import LeftModal from "../PropertiesComponent/PropertiesModalComponent/LeftModal/LeftModal";
 import RightModal from "../PropertiesComponent/PropertiesModalComponent/RightModal/RightModal";
 import AddCommentComponent from "../PropertiesComponent/PropertiesModalComponent/PropertiesCommentModal/AddCommentComponent";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import styles from "./PropertiesModal.module.scss";
-import { setShowDetails } from "../../actions/Actions";
 
-const PropertiesModal = ({ onClose }) => {
+const PropertiesModal = () => {
   const addComments = useSelector((state) => state.show_comment);
-  const dispatch = useDispatch();
-
-  const handleCloseDetails = () => {
-    dispatch(setShowDetails(false));
-  };
 
   return (
     <div className={styles.propertiesModal}>
       {!addComments && <LeftModal />}
-      {!addComments && <RightModal onClose={handleCloseDetails} />}
+      {!addComments && <RightModal />}
       {addComments && <AddCommentComponent />}
     </div>
   );
