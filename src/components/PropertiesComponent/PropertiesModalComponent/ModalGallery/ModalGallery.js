0588dd93356271634adf9fee7c7cfa88b178a4c6
Tobias235 <tobias.prepas@gmail.com>
2022-11-29 16:44:43 +0100

    Seperated increment/decrement

--- src/components/PropertiesComponent/PropertiesModalComponent/ModalGallery/ModalGallery.js
+++ src/components/PropertiesComponent/PropertiesModalComponent/ModalGallery/ModalGallery.js
@@ -1,40 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import styles from "./ModalGallery.module.scss";
 import { ReactComponent as ArrowLeft } from "../../../../assets/images/arrowLeft.svg";
 import { ReactComponent as ArrowRight } from "../../../../assets/images/arrowRight.svg";
 
 const ModalGallery = () => {
+  const [propertyImages, setPropertyImages] = useState();
   const [currentPicture, setCurrentPicture] = useState(6);
 
   const properties = useSelector((state) => state.properties);
   const current_property = useSelector((state) => state.current_property);
 
-  const currentProperty = Object.entries(properties).filter(
-    (property) => property[0] === current_property
-  );
+  const propertyImagesLength = propertyImages?.length - 1;
 
-  const handleImageSlider = (e) => {
-    const propertyImagesLength = currentProperty[0][1].images.length - 1;
-    if (currentPicture === propertyImagesLength && e.target.id === "increment")
+  useEffect(() => {
+    const currentProperty = Object.entries(properties).filter(
+      (property) => property[0] === current_property
+    );
+    setPropertyImages(currentProperty[0][1].images);
+  }, [properties, current_property]);
+
+  const handleIncrement = (e) => {
+    if (
+      currentPicture === propertyImagesLength &&
+      e.target.id === "increment"
+    ) {
       return setCurrentPicture(0);
-    if (currentPicture === 0 && e.target.id === "decrement")
-      return setCurrentPicture(propertyImagesLength);
+    }
+    setCurrentPicture(currentPicture + 1);
+  };
 
-    return e.target.id === "decrement"
-      ? setCurrentPicture(currentPicture - 1)
-      : setCurrentPicture(currentPicture + 1);
+  const handleDecrement = (e) => {
+    if (currentPicture === 0 && e.target.id === "decrement") {
+      return setCurrentPicture(propertyImagesLength);
+    }
+
+    return setCurrentPicture(currentPicture - 1);
   };
 
   return (
     <div className={styles.gallery}>
       <div className={styles.arrowContainer}>
         <div className={styles.arrowInnerContainer}>
-          <ArrowLeft id="decrement" onClick={handleImageSlider} />
-          <ArrowRight id="increment" onClick={handleImageSlider} />
+          <ArrowLeft id="decrement" onClick={handleDecrement} />
+          <ArrowRight id="increment" onClick={handleIncrement} />
         </div>
       </div>
-      <img src={currentProperty[0][1].images[currentPicture]} alt="property" />
+      {propertyImages && (
+        <img src={propertyImages[currentPicture]} alt="property" />
+      )}
     </div>
   );
 };
