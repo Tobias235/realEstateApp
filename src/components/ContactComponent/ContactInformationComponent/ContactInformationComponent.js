abfc9947afef558e846b70f4a5c52455df444950
Tobias235 <tobias.prepas@gmail.com>
2022-12-12 21:05:57 +0100

    Removed react-icons library
    Added icons as SVG to increase performance of website

--- src/components/ContactComponent/ContactInformationComponent/ContactInformationComponent.js
+++ src/components/ContactComponent/ContactInformationComponent/ContactInformationComponent.js
@@ -1,5 +1,5 @@
-import { FaRegEnvelope } from "react-icons/fa";
-import { FiPhone } from "react-icons/fi";
+import { ReactComponent as CallIcon } from "../../../assets/images/callIcon.svg";
+import { ReactComponent as MessageIcon } from "../../../assets/images/messageIcon.svg";
 
 import styles from "./ContactInformationComponent.module.scss";
 import contactUs from "../../../assets/images/contactUs.webp";
@@ -8,11 +8,11 @@ const ContactInformationComponent = () => {
   return (
     <div className={styles.contactInformation}>
       <a href="mailto:presmiy@gmail.com" className={styles.email}>
-        <FaRegEnvelope />
+        <MessageIcon />
         <span>Write us an email</span>
       </a>
       <a href="tel:123456789" className={styles.phone}>
-        <FiPhone />
+        <CallIcon />
         <span>Call us at +123456789</span>
       </a>
       <img src={contactUs} alt="Contact us" className={styles.img} />
