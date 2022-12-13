24cfdc113ddc07990aca25f9fdc71817c8153755
Tobias235 <tobias.prepas@gmail.com>
2022-12-13 16:22:35 +0100

    Moved to modal container

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,37 +1,15 @@
-import { useDispatch, useSelector } from "react-redux";
 import ReactDOM from "react-dom";
-import {
-  setMobileMenu,
-  setShowAddComment,
-  setShowDetails,
-  setShowLoginModal,
-  setShowMobileNavOptions,
-  setShowPropertiesModal,
-} from "../../../actions/Actions";
 import styles from "./Backdrop.module.scss";
 
 const Backdrop = (props) => {
-  const dispatch = useDispatch();
-  const loading = useSelector((state) => state.loading);
   const portalElement = document.getElementById("overlays");
-
-  const onClose = () => {
-    if (loading) return;
-
-    dispatch(setShowLoginModal(false));
-    dispatch(setShowDetails(false));
-    dispatch(setShowAddComment(false));
-    dispatch(setShowPropertiesModal(false));
-    dispatch(setMobileMenu(false));
-    dispatch(setShowMobileNavOptions(false));
-  };
 
   return (
     <>
       {ReactDOM.createPortal(
         <div
           className={`${styles.background} ${props.className}`}
-          onClick={onClose}
+          onClick={props.onClick}
         >
           {props.children}
         </div>,
