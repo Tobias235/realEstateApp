b966edfb374ee475af44fdf599b9fbc0d660200a
Tobias235 <tobias.prepas@gmail.com>
2022-12-19 19:46:50 +0100

    Added edit button
    Edit button for property that puts the current property values in upload form, only shows button if user ID is equal to specific ID

--- src/components/PropertiesModal/RightModal/RightModal.js
+++ src/components/PropertiesModal/RightModal/RightModal.js
@@ -1,5 +1,8 @@
-import { setShowDetails } from "../../../actions/Actions";
-import { useDispatch } from "react-redux";
+import {
+  setShowDetails,
+  setShowUploadPropertiesModal,
+} from "../../../actions/Actions";
+import { useDispatch, useSelector } from "react-redux";
 import ModalDescription from "../ModalDescription/ModalDescription";
 import Button from "../../UI/Button/Button";
 import styles from "./RightModal.module.scss";
@@ -7,18 +10,32 @@ import styles from "./RightModal.module.scss";
 const RightModal = () => {
   const dispatch = useDispatch();
 
-  const handleCloseDetails = () => {
+  const uid = useSelector((state) => state.current_user_id);
+
+  const handleEditProperty = () => {
     dispatch(setShowDetails(false));
+    dispatch(setShowUploadPropertiesModal(true));
   };
+
+  const showEditButton =
+    process.env.REACT_APP_ADMIN_USER === uid ? true : false;
 
   return (
     <div className={styles.rightModal}>
       <h1>Description</h1>
       <ModalDescription />
       <div className={styles.buttonContainer}>
+        {showEditButton && (
+          <Button
+            type="button"
+            onClick={handleEditProperty}
+            text="Edit Property"
+            className={styles.closeButton}
+          />
+        )}
         <Button
           type="button"
-          onClick={handleCloseDetails}
+          onClick={() => dispatch(setShowDetails(false))}
           text="Close Details"
           className={styles.closeButton}
         />
