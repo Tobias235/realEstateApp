d147b88fe04bf8ccdbee57b10810f26383f940a0
Tobias235 <tobias.prepas@gmail.com>
2023-01-10 20:43:41 +0100

    Fixed dispatching
    Added switch case for dispatching and added mobile navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import PropertiesContainer from "./containers/PropertiesContainer/PropertiesContainer";
 import AboutContainer from "./containers/AboutContainer/AboutContainer";
 import ContactContainer from "./containers/ContactContainer/ContactContainer";
@@ -7,42 +6,16 @@ import NavbarContainer from "./containers/NavbarContainer/NavbarContainer";
 import Footer from "./components/Footer/Footer";
 import MobileNavbar from "./components/NavBarComponent/MobileNavbarComponents/MobileNavbar/MobileNavbar";
 import PopupNotifications from "./components/UI/PopupNotifcation/PopupNotification";
-import MobileNavContainer from "./containers/NavbarContainer/MobileNavContainer";
-import { useDispatch, useSelector } from "react-redux";
-import { setShowLoginModal } from "./actions/Actions";
 // import styles from "./App.module.scss";
 import ModalContainer from "./containers/ModalContainer/ModalContainer";
 
 function App() {
-  const [showLoginMenu, setShowLoginMenu] = useState(false);
-
-  const mobile_menu = useSelector((state) => state.mobile_menu);
-
-  const dispatch = useDispatch();
-
-  const handleCloseMenu = () => {
-    dispatch(setShowLoginModal(false));
-  };
-
-  const handleShowLogin = () => {
-    if (showLoginMenu) {
-      setShowLoginMenu(false);
-    }
-    setShowLoginMenu(true);
-  };
-
   return (
     <>
-      <ModalContainer />
       <NavbarContainer />
-      <MobileNavbar onClose={handleCloseMenu} />
-      {mobile_menu && (
-        <MobileNavContainer
-          onClose={handleCloseMenu}
-          onSignIn={handleShowLogin}
-        />
-      )}
+      <MobileNavbar />
       <PopupNotifications />
+      <ModalContainer />
       <HomeContainer />
       <PropertiesContainer />
       <AboutContainer />
--- src/containers/ModalContainer/ModalContainer.js
+++ src/containers/ModalContainer/ModalContainer.js
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
+  setCurrentPropertyData,
   setMobileMenu,
+  setModalName,
   setShowAddComment,
   setShowDetails,
   setShowLoginModal,
-  setShowMobileNavOptions,
+  setShowMobileUserDropdown,
   setShowUploadPropertiesModal,
 } from "../../actions/Actions";
 import Backdrop from "../../components/UI/Backdrop/Backdrop";
@@ -16,10 +18,12 @@ import PropertiesModal from "../../components/PropertiesModal/PropertiesModal";
 import UploadPropertiesModal from "../../components/UploadPropertiesModal/UploadPropertiesModal";
 import Loader from "../../components/UI/Loader/Loader";
 import styles from "./ModalContainer.module.scss";
+import MobileNavContainer from "../NavbarContainer/MobileNavContainer";
 
 const ModalContainer = () => {
   const [modal, setModal] = useState([]);
   const dispatch = useDispatch();
+
   const login_modal = useSelector((state) => state.login_modal);
   const show_upload_properties_modal = useSelector(
     (state) => state.show_upload_properties_modal
@@ -27,26 +31,54 @@ const ModalContainer = () => {
   const show_details = useSelector((state) => state.show_details);
   const loading = useSelector((state) => state.loading);
   const upload_status = useSelector((state) => state.uploading_status);
+  const mobile_menu = useSelector((state) => state.mobile_menu);
+  const modalName = useSelector((state) => state.modalName);
 
   useEffect(() => {
     let stateArray = [];
-    stateArray.push(login_modal, show_upload_properties_modal, show_details);
+    stateArray.push(
+      login_modal,
+      show_upload_properties_modal,
+      show_details,
+      mobile_menu
+    );
     setModal(stateArray.some((val) => val === true));
-  }, [login_modal, show_upload_properties_modal, show_details]);
+  }, [login_modal, show_upload_properties_modal, show_details, mobile_menu]);
 
   const onClose = () => {
     if (loading) return;
 
-    dispatch(setShowLoginModal(false));
-    dispatch(setShowDetails(false));
-    dispatch(setShowAddComment(false));
-    dispatch(setShowUploadPropertiesModal(false));
-    dispatch(setMobileMenu(false));
-    dispatch(setShowMobileNavOptions(false));
+    switch (modalName) {
+      case "login":
+        dispatch(setShowLoginModal(false));
+        break;
+      case "details":
+        dispatch(setShowDetails(false));
+        dispatch(setCurrentPropertyData(null));
+        dispatch(setCurrentPropertyData(null));
+        break;
+      case "upload":
+        dispatch(setShowUploadPropertiesModal(false));
+        break;
+      case "comment":
+        dispatch(setShowAddComment(false));
+        break;
+      case "mobile":
+        dispatch(setMobileMenu(false));
+        dispatch(setShowMobileUserDropdown(false));
+        break;
+      default:
+        return;
+    }
+
+    if (modalName !== "") {
+      dispatch(setModalName(""));
+    }
   };
 
   return (
     <>
+      {mobile_menu && <MobileNavContainer />}
       {modal && !loading && <Backdrop onClick={onClose} />}
       {modal && loading && (
         <Backdrop className={styles.backdropLoader} onClick={onClose}>
