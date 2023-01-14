22ca30ffd5a42d3ba7311d27f906ef841865f6f2
Tobias235 <tobias.prepas@gmail.com>
2023-01-14 21:04:10 +0100

    Updated starRating
    Changed to SVG and made it re-useaable in property card component and add review

--- src/components/PropertiesModal/PropertiesCommentModal/StarRating/StarRating.js
+++ src/components/PropertiesModal/PropertiesCommentModal/StarRating/StarRating.js
@@ -1,36 +1,59 @@
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setRating } from "../../../../actions/Actions";
 import styles from "./StarRating.module.scss";
+import { ReactComponent as Star } from "../../../../assets/images/star.svg";
 
-const StarRating = () => {
+const StarRating = ({ rating, className, isHoverable, isClickable }) => {
+  const [percentValue, setPercentValue] = useState();
   const [hover, setHover] = useState(0);
-  const rating = useSelector((state) => state.rating);
+
+  const ratingValue = useSelector((state) => state.rating);
 
   const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (rating) {
+      const ratingPercent = (rating / 5) * 100;
+      const percentValue = 100 - ratingPercent;
+      setPercentValue(percentValue);
+    }
+  }, [rating]);
+
   return (
-    <div className={styles.starRating}>
-      {[...Array(5)].map((star, i) => {
-        i += 1;
-        return (
-          <button
-            key={i}
-            className={i <= (hover || rating) ? styles.active : styles.inactive}
-            onClick={() => {
-              dispatch(setRating(i));
-            }}
-            onDoubleClick={() => {
-              dispatch(setRating(0));
-              setHover(0);
-            }}
-            onMouseEnter={() => setHover(i)}
-            onMouseLeave={() => setHover(rating)}
-          >
-            &#9733;
-          </button>
-        );
-      })}
+    <div className={`${styles.stars} ${className}`}>
+      {Array(5)
+        .fill()
+        .map((_, i) => {
+          i += 1;
+          return (
+            <Fragment key={i}>
+              {isHoverable && isClickable ? (
+                <Star
+                  className={
+                    i <= (hover || ratingValue)
+                      ? styles.active
+                      : styles.inactive
+                  }
+                  onClick={() => {
+                    dispatch(setRating(i));
+                  }}
+                  onDoubleClick={() => {
+                    dispatch(setRating(0));
+                    setHover(0);
+                  }}
+                  onMouseEnter={() => setHover(i)}
+                  onMouseLeave={() => setHover(ratingValue)}
+                />
+              ) : (
+                <Star />
+              )}
+            </Fragment>
+          );
+        })}
+      <div className={styles.coverContainer}>
+        <span className={styles.cover} style={{ width: percentValue }}></span>
+      </div>
     </div>
   );
 };
