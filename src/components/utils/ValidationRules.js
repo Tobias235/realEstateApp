2b2eb5900a065f7d9ebb3aaf39af920ef0078e90
Tobias235 <tobias.prepas@gmail.com>
2023-01-16 21:57:14 +0100

    Added login validation

--- src/components/utils/ValidationRules.js
+++ src/components/utils/ValidationRules.js
@@ -1,3 +1,14 @@
+export const loginValidation = {
+  email: (value) =>
+    !/\S+@\S+\.\S+/.test(value)
+      ? "Please enter a valid e-mail address."
+      : undefined,
+  password: (value) =>
+    value.length < 8
+      ? "Please enter a password with at least 8 characters."
+      : undefined,
+};
+
 export const contactValidation = {
   name: (value) => (value.length < 5 ? "Please enter your name." : undefined),
   email: (value) =>
