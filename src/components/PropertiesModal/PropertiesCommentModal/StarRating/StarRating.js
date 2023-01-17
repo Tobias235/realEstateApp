a66cc203bec6471c5fbbe64a240001da925a866a
Tobias235 <tobias.prepas@gmail.com>
2023-01-17 22:50:12 +0100

    Update StarRating.js
    Update functioanlity with addReview component with new state management and passing star index to addreview component

--- src/components/PropertiesModal/PropertiesCommentModal/StarRating/StarRating.js
+++ src/components/PropertiesModal/PropertiesCommentModal/StarRating/StarRating.js
@@ -1,49 +1,40 @@
 import { Fragment, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setRating } from "../../../../actions/Actions";
 import styles from "./StarRating.module.scss";
 import { ReactComponent as Star } from "../../../../assets/images/star.svg";
 
-const StarRating = ({ rating, className, isHoverable, isClickable }) => {
+const StarRating = (props) => {
   const [percentValue, setPercentValue] = useState();
   const [hover, setHover] = useState(0);
 
-  const ratingValue = useSelector((state) => state.rating);
-
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    if (rating) {
-      const ratingPercent = (rating / 5) * 100;
+    if (props.rating) {
+      const ratingPercent = (props.rating / 5) * 100;
       const percentValue = 100 - ratingPercent;
       setPercentValue(percentValue);
     }
-  }, [rating]);
+  }, [props.rating]);
 
   return (
-    <div className={`${styles.stars} ${className}`}>
+    <div className={`${styles.stars} ${props.className}`}>
       {Array(5)
         .fill()
         .map((_, i) => {
           i += 1;
           return (
             <Fragment key={i}>
-              {isHoverable && isClickable ? (
+              {props.isHoverable && props.isClickable ? (
                 <Star
                   className={
-                    i <= (hover || ratingValue)
+                    i <= (hover || props.starRating)
                       ? styles.active
                       : styles.inactive
                   }
+                  name={props.name}
                   onClick={() => {
-                    dispatch(setRating(i));
-                  }}
-                  onDoubleClick={() => {
-                    dispatch(setRating(0));
-                    setHover(0);
+                    props.onClick(i);
                   }}
                   onMouseEnter={() => setHover(i)}
-                  onMouseLeave={() => setHover(ratingValue)}
+                  onMouseLeave={() => setHover(props.starRating)}
                 />
               ) : (
                 <Star />
