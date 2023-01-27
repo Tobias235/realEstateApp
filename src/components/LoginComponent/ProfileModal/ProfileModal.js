a69f5b21c5a60ba880e0ef6828f7ab3720eefe49
Tobias235 <tobias.prepas@gmail.com>
2023-01-27 12:15:10 +0100

    created show profile component

---
+++ src/components/LoginComponent/ProfileModal/ProfileModal.js
@@ -0,0 +1,9 @@
+import Modal from "../../UI/Modal/Modal";
+
+import styles from "./ProfileModal.module.scss";
+
+const ProfileModal = () => {
+  return <Modal></Modal>;
+};
+
+export default ProfileModal;
