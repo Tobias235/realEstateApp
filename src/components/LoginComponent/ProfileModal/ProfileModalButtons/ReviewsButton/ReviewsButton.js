00d385ac6694b7e4e380023ccecc878cbf51ab52
Tobias235 <tobias.prepas@gmail.com>
2023-01-28 23:04:41 +0100

    Create ReviewsButton.js

---
+++ src/components/LoginComponent/ProfileModal/ProfileModalButtons/ReviewsButton/ReviewsButton.js
@@ -0,0 +1,9 @@
+import Button from "../../../../UI/Button/Button";
+
+const ReviewsButton = () => {
+  const handleShowReviews = () => {};
+
+  return <Button text="reviews" onClick={handleShowReviews} />;
+};
+
+export default ReviewsButton;
