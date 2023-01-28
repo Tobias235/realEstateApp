394f62e7eba450dec063d0bc7a59c44367577c9a
Tobias235 <tobias.prepas@gmail.com>
2023-01-28 23:04:07 +0100

    Added components
    Change email/password buttons to render ChangeForm component,
    
    delete button to delete account
    
    Review button to view past reviews made by yourself

--- src/components/LoginComponent/ProfileModal/ProfileModal.js
+++ src/components/LoginComponent/ProfileModal/ProfileModal.js
@@ -1,9 +1,44 @@
-import Modal from "../../UI/Modal/Modal";
-
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Button from "../../UI/Button/Button";
 import styles from "./ProfileModal.module.scss";
+import DeleteAccountButton from "./ProfileModalButtons/DeleteAccountButton/DeleteAccountButton";
+import ReviewsButton from "./ProfileModalButtons/ReviewsButton/ReviewsButton";
+import ChangeForm from "./UpdateProfileForms/ChangeForm/ChangeForm";
 
 const ProfileModal = () => {
-  return <Modal></Modal>;
+  const [formType, setFormType] = useState("");
+
+  const user = useSelector((state) => state.name);
+
+  const closeModal = () => {
+    setFormType("");
+  };
+
+  return (
+    <div className={styles.profileModal}>
+      <h1>Welcome, {user}</h1>
+      <ReviewsButton />
+      <Button
+        type="button"
+        text="change email"
+        onClick={() => setFormType("email")}
+      />
+      <Button
+        type="button"
+        text="change password"
+        onClick={() => setFormType("password")}
+      />
+      <DeleteAccountButton />
+
+      {formType !== "" && (
+        <Backdrop className={styles.backDrop}>
+          <ChangeForm formType={formType} closeModal={closeModal} />
+        </Backdrop>
+      )}
+    </div>
+  );
 };
 
 export default ProfileModal;
