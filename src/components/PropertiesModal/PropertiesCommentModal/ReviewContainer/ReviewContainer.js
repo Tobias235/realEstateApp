41c6afd3ace7bbb291a112c8e67deb65bfb12657
Tobias235 <tobias.prepas@gmail.com>
2023-02-01 22:15:13 +0100

    moved into reviewContainer

--- src/components/PropertiesModal/PropertiesCommentModal/ReviewContainer/ReviewContainer.js
+++ src/components/PropertiesModal/PropertiesCommentModal/ReviewContainer/ReviewContainer.js
@@ -1,10 +1,17 @@
-import ModalCommentCard from "../ModalCommentCard/ModalCommentCard";
+import { useSelector } from "react-redux";
+import ReviewCard from "../../../UI/ReviewCard/ReviewCard";
 import styles from "./ReviewContainer.module.scss";
 
 const ReviewContainer = () => {
+  const currentPropertyData = useSelector(
+    (state) => state.current_property_data
+  );
   return (
     <div className={styles.reviewContainer}>
-      <ModalCommentCard />
+      {currentPropertyData.reviews &&
+        Object.entries(currentPropertyData.reviews).map(([key, review]) => {
+          return <ReviewCard review={review} key={key} />;
+        })}
     </div>
   );
 };
