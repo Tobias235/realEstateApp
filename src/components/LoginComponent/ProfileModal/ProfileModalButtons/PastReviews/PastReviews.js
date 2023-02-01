1a95c50c5634dcc0e65a7acc25960bc382eb3aed
Tobias235 <tobias.prepas@gmail.com>
2023-02-01 22:16:00 +0100

    Added past reviews
    Shows past reviews made by your speicifc UId

--- src/components/LoginComponent/ProfileModal/ProfileModalButtons/PastReviews/PastReviews.js
+++ src/components/LoginComponent/ProfileModal/ProfileModalButtons/PastReviews/PastReviews.js
@@ -1,5 +1,37 @@
-const PastReviews = () => {
-  return <div></div>;
+import { useSelector } from "react-redux";
+import { auth } from "../../../../../Firebase";
+import Button from "../../../../UI/Button/Button";
+import Modal from "../../../../UI/Modal/Modal";
+import ReviewCard from "../../../../UI/ReviewCard/ReviewCard";
+import styles from "./PastReviews.module.scss";
+
+const PastReviews = (props) => {
+  const properties = useSelector((state) => state.properties);
+
+  const userUID = auth.currentUser.uid;
+
+  const propertyValues = Object.values(properties);
+
+  const reviews = [].concat(
+    ...propertyValues.map((property) => {
+      if (!property.reviews) {
+        return [];
+      }
+      return Object.values(property.reviews).filter(
+        (review) => review.uid === userUID
+      );
+    })
+  );
+
+  return (
+    <Modal className={styles.pastReviewsContainer}>
+      <h1>Your reviews</h1>
+      {reviews.map((review, i) => {
+        return <ReviewCard review={review} key={i} />;
+      })}
+      <Button type="button" text="Close" onClick={props.closeModal} />
+    </Modal>
+  );
 };
 
 export default PastReviews;
