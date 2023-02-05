f8696f1ed9028bdbb06ec81d55e984d1214c3122
Tobias235 <tobias.prepas@gmail.com>
2023-02-05 12:29:47 +0100

    seperated desktop/mobile component

--- src/components/PropertiesModal/PropertiesModal.js
+++ src/components/PropertiesModal/PropertiesModal.js
@@ -1,22 +1,11 @@
-import LeftModal from "./LeftModal/LeftModal";
-import RightModal from "./RightModal/RightModal";
-import AddReviewComponent from "../PropertiesModal/PropertiesCommentModal/AddReviewComponent/AddReviewComponent";
-import { useSelector } from "react-redux";
-import styles from "./PropertiesModal.module.scss";
+import DesktopDetails from "./DesktopDetails/DesktopDetails";
+// import styles from "./PropertiesModal.module.scss";
 
 const PropertiesModal = () => {
-  const addComments = useSelector((state) => state.show_comment);
-
-  const detailsReviewStyling = addComments
-    ? styles.addReview
-    : styles.propertiesModal;
-
   return (
-    <div className={detailsReviewStyling}>
-      {!addComments && <LeftModal />}
-      {!addComments && <RightModal />}
-      {addComments && <AddReviewComponent />}
-    </div>
+    <>
+      <DesktopDetails />
+    </>
   );
 };
 
