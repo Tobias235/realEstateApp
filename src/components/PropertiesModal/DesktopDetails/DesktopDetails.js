84f180f953060ab0919229206d8dc5ce2625aa8c
Tobias235 <tobias.prepas@gmail.com>
2023-02-05 12:29:11 +0100

    Seperated for desktop

---
+++ src/components/PropertiesModal/DesktopDetails/DesktopDetails.js
@@ -0,0 +1,23 @@
+import LeftModal from "../LeftModal/LeftModal";
+import RightModal from "../RightModal/RightModal";
+import AddReviewComponent from "../../PropertiesModal/PropertiesCommentModal/AddReviewComponent/AddReviewComponent";
+import { useSelector } from "react-redux";
+import styles from "./DesktopDetails.module.scss";
+
+const DesktopDetails = () => {
+  const addComments = useSelector((state) => state.show_comment);
+
+  const detailsReviewStyling = addComments
+    ? styles.addReview
+    : styles.propertiesModal;
+
+  return (
+    <div className={detailsReviewStyling}>
+      {!addComments && <LeftModal />}
+      {!addComments && <RightModal />}
+      {addComments && <AddReviewComponent />}
+    </div>
+  );
+};
+
+export default DesktopDetails;
