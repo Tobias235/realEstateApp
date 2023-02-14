9c8c8fe40bcfe460592ae46f1eae8ae7bd5ee3de
Tobias235 <tobias.prepas@gmail.com>
2023-02-14 20:11:41 +0100

    Added configure store from redux toolkit and persist state

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,36 @@
-import { createStore } from "redux";
-import Reducer from "./reducers/Reducer";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import sessionStorage from "redux-persist/lib/storage/session";
+import UserReducer from "./reducers/UserReducer";
+import ModalReducer from "./reducers/ModalReducer";
+import PropertyReducer from "./reducers/PropertyReducer";
+import FilterReducer from "./reducers/FilterReducer";
+import LoadingReducer from "./reducers/LoadingReducer";
+import MobileReducer from "./reducers/MobileReducer";
+const persistConfig = {
+  key: "storage",
+  storage: sessionStorage,
+};
 
-//Creating store and providing state to the app through index.js file and an redux extension devtool
-//for better visibility of what is happening
-const Store = createStore(
-  Reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-);
+const rootReducer = combineReducers({
+  userReducer: UserReducer,
+  modalReducer: ModalReducer,
+  propertyReducer: PropertyReducer,
+  filterReducer: FilterReducer,
+  loadingReducer: LoadingReducer,
+  mobileReducer: MobileReducer,
+});
 
-export default Store;
+const store = configureStore({
+  reducer: persistReducer(persistConfig, rootReducer),
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }).concat(),
+
+  devTools: process.env.NODE_ENV === "development",
+});
+
+const persistor = persistStore(store);
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { store, persistor };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import Store from "./Store";
+import { PersistGate } from "redux-persist/integration/react";
+import store from "./Store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={Store}>
-    <App />
+  <Provider store={store.store}>
+    <PersistGate persistor={store.persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
